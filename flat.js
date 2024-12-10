02cb53f4eec34d3877411e050073505305d1c82f
Amit kapoor <32587722+Amitk3004@users.noreply.github.com>
2024-12-10 12:41:58 +0530

    Create flat.js

---
+++ flat.js
@@ -0,0 +1,22 @@
+
+function flat(arr){
+  // creating a new array to hold values
+  const update = [];
+
+  // logic to flat a nested array using recursion
+  arr.forEach((e) => {
+    if(Array.isArray(e)){
+      update.push(...flat(e));
+    } else {
+      update.push(e);
+    }
+  });
+  
+  // return final array 
+  return update;
+}
+
+// driver funciton 
+let nestedArray = [1,2,[3,4],[5,6,[7,8]]];
+const finalArray = flat(nestedArray);
+console.log(finalArray);
